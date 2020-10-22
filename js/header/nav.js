f6c6e47119e79d8dae852a891d8031fe9d812f04
zhumadil-sjs <zhumadil.seiitkulov01@gmail.com>
2020-10-22 16:56:19 +0600

    header

--- js/header/nav.js
+++ js/header/nav.js
@@ -1,24 +1,24 @@
-import { nav, ul, navLinks } from "./const.js";
+import { nav, ul, navLinks, colorGray } from './const.js';
 
 nav.appendChild(ul);
+ul.style.display = 'flex'
 for (let i = 0; i < navLinks.length; i++) {
-  ul.style.display = "flex";
-  let li = document.createElement("li");
-  li.style.listStyle = "none";
-  li.style.marginLeft = "35px";
-  let a = document.createElement("a");
-  a.innerHTML = `${navLinks[i]}`;
-  a.href = "#";
-  a.className = "nav__links";
-  a.style.textDecoration = "none";
-  a.style.color = "#7b7b7b";
-  a.style.fontSize = "12px";
-  a.style.letterSpacing = "2px";
-  a.style.lineHeight = "30px";
-  a.style.fontFamily = "pappins";
-  a.style.textTransform = "uppercase";
-  li.appendChild(a);
+	let li = document.createElement('li');
+	li.style.listStyle = 'none';
+	li.style.marginLeft = '35px';
+	let a = document.createElement('a');
+	a.innerHTML = `${navLinks[i]}`;
+	a.href = '#';
+	a.className = 'nav__links';
+	a.style.textDecoration = 'none';
+	a.style.color = colorGray;
+	a.style.fontSize = '12px';
+	a.style.letterSpacing = '2px';
+	a.style.lineHeight = '30px';
+	a.style.fontFamily = 'Poppins';
+	a.style.textTransform = 'uppercase';
+	li.appendChild(a)
   ul.appendChild(li);
-
 }
-export default nav;
+
+export default nav; 
--- js/header/styles/dropdown.js
+++ js/header/styles/dropdown.js
@@ -1,27 +1,28 @@
-import { dropDown, span } from "../const.js";
+import { dropDown, colorGray } from '../const.js';
 
-const arrow = span;
-
+export const arrow = document.createElement('span');
 
 arrow.innerHTML = `>`;
-arrow.style.position = "absolute";
-arrow.style.right = "0";
-arrow.style.transform = "rotate(90deg)";
+arrow.style.position = 'absolute';
+arrow.style.right = '0';
+arrow.style.transform = 'rotate(90deg)';
+arrow.style.transition = '0.3s'
 dropDown.appendChild(arrow);
-dropDown.style.boxSizing = "border-box";
-dropDown.style.cursor = "pointer";
-dropDown.style.position = "relative";
-dropDown.style.padding = "15px 15px 15px 0";
-dropDown.style.margin = "0";
-dropDown.style.listStyle = "none";
-dropDown.style.height = "60px";
-dropDown.style.overflow = "hidden";
-dropDown.style.textDecoration = "none";
-dropDown.style.color = "#7b7b7b";
-dropDown.style.fontSize = "12px";
-dropDown.style.letterSpacing = "2px";
-dropDown.style.lineHeight = "30px";
-dropDown.style.fontFamily = "Poppins";
-dropDown.style.textTransform = "uppercase";
+dropDown.style.boxSizing = 'border-box';
+dropDown.style.cursor = 'pointer';
+dropDown.style.position = 'relative';
+dropDown.style.padding = '15px 15px 15px 0';
+dropDown.style.margin = '0';
+dropDown.style.listStyle = 'none';
+dropDown.style.height = '60px';
+dropDown.style.overflow = 'hidden';
+dropDown.style.textDecoration = 'none';
+dropDown.style.color = colorGray;
+dropDown.style.fontSize = '12px';
+dropDown.style.letterSpacing = '2px';
+dropDown.style.lineHeight = '30px';
+dropDown.style.fontFamily = 'Poppins';
+dropDown.style.textTransform = 'uppercase';
+
 
 export default dropDown;
