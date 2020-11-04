6fc94673e48ce03558c2399bfc6530c3261fa5c8
zhumadil-sjs <zhumadil.seiitkulov01@gmail.com>
2020-11-05 01:48:08 +0600

    good style code

--- js/components/header/nav.js
+++ js/components/header/nav.js
@@ -1,22 +1,22 @@
-import { nav, ul, navLinks, colorGray, navLinksLocation } from './const.js';
+import { nav, ul, navLinks, colorGray, navLinksLocation } from "./const.js";
 
 nav.appendChild(ul);
-ul.style.display = 'flex';
+ul.style.display = "flex";
 for (let i = 0; i < navLinks.length; i++) {
-  let li = document.createElement('li');
-  li.style.listStyle = 'none';
-  li.style.marginLeft = '35px';
-  let a = document.createElement('a');
+  let li = document.createElement("li");
+  li.style.listStyle = "none";
+  li.style.marginLeft = "35px";
+  let a = document.createElement("a");
   a.innerHTML = `${navLinks[i]}`;
   a.href = `#${navLinksLocation[i]}`;
-  a.className = 'nav__links';
-  a.style.textDecoration = 'none';
+  a.className = "nav__links";
+  a.style.textDecoration = "none";
   a.style.color = colorGray;
-  a.style.fontSize = '12px';
-  a.style.letterSpacing = '2px';
-  a.style.lineHeight = '30px';
-  a.style.fontFamily = 'Poppins';
-  a.style.textTransform = 'uppercase';
+  a.style.fontSize = "12px";
+  a.style.letterSpacing = "2px";
+  a.style.lineHeight = "30px";
+  a.style.fontFamily = "Poppins";
+  a.style.textTransform = "uppercase";
   li.appendChild(a);
   ul.appendChild(li);
 }
