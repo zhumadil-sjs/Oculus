c30eceb9b2a1d948c4fb3efb2ee7b87c282fe404
zhumadil-sjs <zhumadil.seiitkulov01@gmail.com>
2020-11-04 22:43:21 +0600

    dropdownitem.js

--- js/components/header/dropDownItem.js
+++ js/components/header/dropDownItem.js
@@ -1,48 +1,48 @@
-import { languages } from './const.js';
-import dropDown from './style/dropdown.js';
-import { arrow } from './style/dropdown.js';
+import { languages } from "./const.js";
+import dropDown from "./style/dropdown.js";
+import { arrow } from "./style/dropdown.js";
 
 let array = [];
 
 for (let i = 0; i < languages.length; i++) {
-  var dropDownItems = document.createElement('li');
-  dropDownItems.style.textTransform = 'uppercase';
+  var dropDownItems = document.createElement("li");
+  dropDownItems.style.textTransform = "uppercase";
   dropDownItems.innerHTML = `${languages[i]}`;
   dropDown.appendChild(dropDownItems);
-  dropDownItems.style.transition = '0.3s';
+  dropDownItems.style.transition = "0.3s";
   array.push(dropDownItems);
   if (i > 0) {
-    dropDownItems.style.opacity = '0';
+    dropDownItems.style.opacity = "0";
   }
 }
 
-dropDown.addEventListener('click', function () {
-  if (dropDown.className == 'is-active') {
-    dropDown.style.height = '60px';
-    dropDown.className = '';
-    dropDown.style.backgroundColor = 'transparent';
-    dropDown.style.top = '0';
-    dropDown.style.zIndex = '0';
+dropDown.addEventListener("click", function () {
+  if (dropDown.className == "is-active") {
+    dropDown.style.height = "60px";
+    dropDown.className = "";
+    dropDown.style.backgroundColor = "transparent";
+    dropDown.style.top = "0";
+    dropDown.style.zIndex = "0";
     array.forEach((item, index) => {
       if (index > 0) {
-        item.style.opacity = '0';
-        item.style.visibility = 'hidden';
+        item.style.opacity = "0";
+        item.style.visibility = "hidden";
       }
     });
-    arrow.style.transform = 'rotate(90deg)';
+    arrow.style.transform = "rotate(90deg)";
   } else {
-    dropDown.style.zIndex = '1';
-    dropDown.style.top = '1.1vw';
-    dropDown.style.height = 'auto';
-    arrow.style.transform = 'rotate(-90deg)';
-    dropDownItems.style.display = 'block';
-    dropDown.className = 'is-active';
-    dropDown.style.overflow = 'visible';
-    dropDown.style.backgroundColor = '#fff';
+    dropDown.style.zIndex = "1";
+    dropDown.style.top = "1.1vw";
+    dropDown.style.height = "auto";
+    arrow.style.transform = "rotate(-90deg)";
+    dropDownItems.style.display = "block";
+    dropDown.className = "is-active";
+    dropDown.style.overflow = "visible";
+    dropDown.style.backgroundColor = "#fff";
     array.forEach((item) => {
-      item.style.opacity = '1';
-      item.style.visibility = 'visible';
+      item.style.opacity = "1";
+      item.style.visibility = "visible";
     });
   }
-  dropDown.style.bottom = '0';
+  dropDown.style.bottom = "0";
 });
--- js/components/header/index.js
+++ js/components/header/index.js
@@ -1,36 +1,35 @@
-import { container } from '../../base.js';
-import { dropDown, header } from './const.js';
-import burgerMenu from './burgerMenu.js';
-import nav from './nav.js';
-import Cart from './cart.js';
-import logo from './logo.js';
-import searchIcon from './search.js'
-import './dropDownItem.js';
+import { container } from "../../base.js";
+import { dropDown, header } from "./const.js";
+import burgerMenu from "./burgerMenu.js";
+import nav from "./nav.js";
+import Cart from "./cart.js";
+import logo from "./logo.js";
+import searchIcon from "./search.js";
+import "./dropDownItem.js";
 
-const headerRow = document.createElement('div');
-const headerBottom = document.createElement('div');
+const headerRow = document.createElement("div");
+const headerBottom = document.createElement("div");
 
+headerBottom.className = "header-bottom";
 
-headerBottom.className = 'header-bottom';
+headerRow.style.display = "flex";
+headerRow.style.justifyContent = "space-between";
+headerRow.style.alignItems = "center";
+headerRow.style.maxHeight = "50px";
 
-headerRow.style.display = 'flex';
-headerRow.style.justifyContent = 'space-between';
-headerRow.style.alignItems = 'center';
-headerRow.style.maxHeight = '50px';
-
-header.className = 'header';
+header.className = "header";
 
 header.appendChild(container);
 header.appendChild(headerRow);
-header.appendChild(headerBottom)
+header.appendChild(headerBottom);
 container.appendChild(headerRow);
-container.appendChild(headerBottom)
+container.appendChild(headerBottom);
 headerRow.appendChild(dropDown);
 headerRow.appendChild(nav);
 headerRow.appendChild(Cart);
 
-headerBottom.appendChild(burgerMenu)
-headerBottom.appendChild(logo)
-headerBottom.appendChild(searchIcon)
+headerBottom.appendChild(burgerMenu);
+headerBottom.appendChild(logo);
+headerBottom.appendChild(searchIcon);
 
 export default header;
